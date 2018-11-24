ddfe67ce15cf817f42f16d971d0f57851ecc758d
creatorlxd <creatorlxd@outlook.com>
2018-11-24 13:58:11 +0800

    fix ps

--- run_test.ps1
+++ run_test.ps1
@@ -4,10 +4,9 @@ $solution_directory=$env:APPVEYOR_BUILD_FOLDER
 
 Get-ChildItem $solution_directory\Test\ | ForEach-Object
 {
-Write-Host "Testing"$_
 	if($_.Attributes -eq "Directory")
 	{
-		Write-Host "Testing"$_
+		Write-Output ("Testing "+$_)
 		copy $solution_directory\Binary\$_\$platform\$configuration\$_.exe $solution_directory\Test\$_\$_.exe
 		&$solution_directory\Test\$_\$_.exe
 	}
