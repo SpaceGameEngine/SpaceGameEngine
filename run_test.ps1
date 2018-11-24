77ff46953494226a70007e4e3f76907ba861440f
creatorlxd <creatorlxd@outlook.com>
2018-11-24 12:56:16 +0800

    use ci

---
+++ run_test.ps1
@@ -0,0 +1,12 @@
+#you can just attend new test project name
+$test_projects=@("TestCatch2")
+
+function run_test($configuration,$platform)
+{
+	for($i=0;$i -le $test_projects.count;$i++)
+	{
+		copy $env:APPVEYOR_BUILD_FOLDER\Binary\$test_projects[i]\$platform\$configuration\$test_projects[i].exe $env:APPVEYOR_BUILD_FOLDER\Test\$test_projects[i]\$test_projects[i].exe
+		$env:APPVEYOR_BUILD_FOLDER\Test\$test_projects[i]\$test_projects[i].exe
+	}
+}
+run_test($env:CONFIGURATION,$env:PLATFORM)
