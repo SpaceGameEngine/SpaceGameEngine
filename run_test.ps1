1f129e806807c1c52ba6d541ed3295ececaa6024
creatorlxd <creatorlxd@outlook.com>
2018-11-24 14:12:27 +0800

    1

--- run_test.ps1
+++ run_test.ps1
@@ -4,8 +4,11 @@ $configuration=$env:CONFIGURATION
 $platform=$env:PLATFORM
 $solution_directory=$env:APPVEYOR_BUILD_FOLDER
 
+#test
+Write-Output $solution_directory
 Get-ChildItem $solution_directory\Test\ | ForEach-Object
 {
+    Write-Output $_.BaseName
 	if($_.Attributes -eq "Directory")
 	{
 		$test_name=$_.BaseName
