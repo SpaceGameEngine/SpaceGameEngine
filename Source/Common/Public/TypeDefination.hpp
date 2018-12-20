da2837725bd3dcb0a8aad9790a726e09e88f6a78
creatorlxd <creatorlxd@outlook.com>
2018-12-20 21:08:59 +0800

    add common add common::typedefination

---
+++ Source/Common/Public/TypeDefination.hpp
@@ -0,0 +1,69 @@
+﻿/*
+Copyright 2018 creatorlxd
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+#pragma once
+#include <cstdint>
+
+namespace SpaceGameEngine
+{
+	/*!
+	@ingroup Common
+	@{
+	*/
+
+	/*!
+	@brief 8-bit int
+	*/
+	using Int8 = int8_t;
+	
+	/*!
+	@brief 8-bit unsigned int
+	*/
+	using UInt8 = uint8_t;
+
+	/*!
+	@brief 16-bit int
+	*/
+	using Int16 = int16_t;
+
+	/*!
+	@brief 16-bit unsigned int
+	*/
+	using UInt16 = uint16_t;
+
+	/*!
+	@brief 32-bit int
+	*/
+	using Int32 = int32_t;
+
+	/*!
+	@brief 32-bit unsigned int
+	*/
+	using UInt32 = uint32_t;
+
+	/*!
+	@brief 64-bit int
+	*/
+	using Int64 = int64_t;
+
+	/*!
+	@brief 64-bit unsigned int
+	*/
+	using UInt64 = uint64_t;
+	
+	/*!
+	@}
+	*/
+}
