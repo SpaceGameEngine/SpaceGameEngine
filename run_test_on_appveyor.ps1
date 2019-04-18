087a6d7fd2c1bc080906113b179eabb61d5bb5fc
creatorlxd <creatorlxd@outlook.com>
2019-04-18 23:41:36 +0800

    fix a bug in test script

--- run_test_on_appveyor.ps1
+++ run_test_on_appveyor.ps1
@@ -10,6 +10,6 @@ Get-ChildItem $solution_directory\Test\ | ForEach-Object{
 		$test_name=$_.BaseName
 		Write-Output ("Testing "+$test_name)
 		cd $solution_directory\Test\$test_name
-		$solution_directory\Binary\$test_name\$platform\$configuration\$test_name.exe
+		&$solution_directory\Binary\$test_name\$platform\$configuration\$test_name.exe
 	}
 }
