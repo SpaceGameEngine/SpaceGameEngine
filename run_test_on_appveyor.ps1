c9a8e679820b4354a6d33dee895b6a0d40189af3
徐晨曦 <xcx14@outlook.com>
2019-08-15 16:49:17 +0800

    Update run_test_on_appveyor.ps1

--- run_test_on_appveyor.ps1
+++ run_test_on_appveyor.ps1
@@ -12,7 +12,7 @@ Get-ChildItem $solution_directory\Test\ | ForEach-Object{
 		$test_name=$_.BaseName
 		Write-Output ("Testing "+$test_name)
 		cd $solution_directory\Test\$test_name
-		& $solution_directory\Binary\$test_name\$platform\$configuration\$test_name.exe -r junit -o $test_name.xml
-		$wc.UploadFile("https://ci.appveyor.com/api/testresults/junit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\$test_name.xml))
+		& $solution_directory\Binary\$test_name\$platform\$configuration\$test_name.exe -r junit -o test_result.xml
+		$wc.UploadFile("https://ci.appveyor.com/api/testresults/junit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path test_result.xml))
 	}
 }
