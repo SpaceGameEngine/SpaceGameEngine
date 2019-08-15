7e115684254a8ef37f38b78a21d6d5d3b12db07e
徐晨曦 <xcx14@outlook.com>
2019-08-15 16:36:01 +0800

    Update run_test_on_appveyor.ps1

--- run_test_on_appveyor.ps1
+++ run_test_on_appveyor.ps1
@@ -4,12 +4,15 @@ $configuration=$env:CONFIGURATION
 $platform=$env:PLATFORM
 $solution_directory=$env:APPVEYOR_BUILD_FOLDER
 
+$wc = New-Object 'System.Net.WebClient'
+
 Get-ChildItem $solution_directory\Test\ | ForEach-Object{
 	if($_.Attributes -eq "Directory")
 	{
 		$test_name=$_.BaseName
 		Write-Output ("Testing "+$test_name)
 		cd $solution_directory\Test\$test_name
-		&$solution_directory\Binary\$test_name\$platform\$configuration\$test_name.exe
+		& $solution_directory\Binary\$test_name\$platform\$configuration\$test_name.exe -r junit -o $test_name.xml
+		$wc.UploadFile("https://ci.appveyor.com/api/testresults/junit/$($env:APPVEYOR_JOB_ID)", (Resolve-Path .\$test_name.xml))
 	}
 }
