a236e92ef8fe840a0b134a0f38800a0aeee8f942
xsun2001 <1563770452@qq.com>
2019-09-27 14:25:08 +0800

    final fix

--- azure-pipelines/run-tests.sh
+++ azure-pipelines/run-tests.sh
@@ -4,6 +4,7 @@ cd Test
 tests=($(ls))
 
 for test in "${tests[@]}" ; do
-    cd "$test"
-    "../../build/Test/$test/$test" "--gtest_output=xml:$test-REPORT.xml"
+    cd $test
+    ../../build/Test/$test/$test --gtest_output=xml:$test-REPORT.xml
+    cd ..
 done
