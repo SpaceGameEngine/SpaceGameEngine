a7d7a4610a497de2437e4737edbaab640d1eb82e
xsun2001 <1563770452@qq.com>
2019-10-15 00:10:01 +0800

    enable benchmark on ci

---
+++ azure-pipelines/run-benchmarks.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd Benchmark
+benchmarks=($(ls))
+
+for benchmark in "${benchmarks[@]}" ; do
+    cd $benchmark
+    ../../build/Benchmark/$benchmark/$benchmark --benchmark_format=json --benchmark_out=$benchmark-REPORT.json
+    cd ..
+done
