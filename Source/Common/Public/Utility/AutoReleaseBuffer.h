f2e223eee488ab29a32b675d362f448bf264457b
creatorlxd <1461637345@qq.com>
2021-02-05 17:18:29 +0800

    fix license

--- Source/Common/Public/Utility/AutoReleaseBuffer.h
+++ Source/Common/Public/Utility/AutoReleaseBuffer.h
@@ -1,5 +1,5 @@
 ﻿/*
-Copyright 2019 creatorlxd
+Copyright 2021 creatorlxd
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
