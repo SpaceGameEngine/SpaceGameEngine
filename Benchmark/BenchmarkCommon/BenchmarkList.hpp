2c4638493a898e95696acb878dde20af15ac7464
creatorlxd <1461637345@qq.com>
2021-07-26 19:13:49 +0800

    feat : add benchmark for vector and list

---
+++ Benchmark/BenchmarkCommon/BenchmarkList.hpp
@@ -0,0 +1,159 @@
+﻿/*
+Copyright 2021 creatorlxd
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+#pragma once
+#include <benchmark/benchmark.h>
+#include "Container/List.hpp"
+#include <list>
+#include <cassert>
+
+void BM_StdListCreate(benchmark::State& state)
+{
+	for (auto _ : state)
+	{
+		std::list<int> l;
+	}
+}
+
+void BM_SgeListCreate(benchmark::State& state)
+{
+	for (auto _ : state)
+	{
+		SpaceGameEngine::List<int> l;
+	}
+}
+
+BENCHMARK(BM_StdListCreate)->Iterations(1000000);
+BENCHMARK(BM_SgeListCreate)->Iterations(1000000);
+
+inline const int list_test_size = 1024;
+
+void BM_StdListPushBack(benchmark::State& state)
+{
+	for (auto _ : state)
+	{
+		std::list<int> l;
+		for (int i = 0; i < list_test_size; ++i)
+		{
+			l.push_back(i);
+		}
+	}
+}
+
+void BM_SgeListPushBack(benchmark::State& state)
+{
+	for (auto _ : state)
+	{
+		SpaceGameEngine::List<int> l;
+		for (int i = 0; i < list_test_size; ++i)
+		{
+			l.PushBack(i);
+		}
+	}
+}
+
+BENCHMARK(BM_StdListPushBack)->Iterations(1000000);
+BENCHMARK(BM_SgeListPushBack)->Iterations(1000000);
+
+void BM_StdListInsert(benchmark::State& state)
+{
+	for (auto _ : state)
+	{
+		std::list<int> l{0};
+		for (int i = 0; i < list_test_size; ++i)
+		{
+			auto iter = l.begin();
+			++iter;
+			l.insert(iter, i);
+		}
+	}
+}
+
+void BM_SgeListInsert(benchmark::State& state)
+{
+	for (auto _ : state)
+	{
+		SpaceGameEngine::List<int> l{0};
+		for (int i = 0; i < list_test_size; ++i)
+		{
+			auto iter = l.GetBegin();
+			++iter;
+			l.Insert(iter, i);
+		}
+	}
+}
+
+BENCHMARK(BM_StdListInsert)->Iterations(10000);
+BENCHMARK(BM_SgeListInsert)->Iterations(10000);
+
+void BM_StdListRemove(benchmark::State& state)
+{
+	for (auto _ : state)
+	{
+		std::list<int> l(list_test_size, 1);
+		for (int i = 0; i < list_test_size - 1; ++i)
+		{
+			auto iter = l.begin();
+			++iter;
+			l.erase(iter);
+		}
+	}
+}
+
+void BM_SgeListRemove(benchmark::State& state)
+{
+	for (auto _ : state)
+	{
+		SpaceGameEngine::List<int> l(list_test_size, 1);
+		for (int i = 0; i < list_test_size - 1; ++i)
+		{
+			auto iter = l.GetBegin();
+			++iter;
+			l.Remove(iter);
+		}
+	}
+}
+
+BENCHMARK(BM_StdListRemove)->Iterations(10000);
+BENCHMARK(BM_SgeListRemove)->Iterations(10000);
+
+void BM_StdListIteration(benchmark::State& state)
+{
+	for (auto _ : state)
+	{
+		std::list<int> l(list_test_size, 1);
+		int cnt = 0;
+		for (auto i = l.begin(); i != l.end(); ++i, ++cnt)
+		{
+			assert(*i, cnt);
+		}
+	}
+}
+
+void BM_SgeListIteration(benchmark::State& state)
+{
+	for (auto _ : state)
+	{
+		SpaceGameEngine::List<int> l(list_test_size, 1);
+		int cnt = 0;
+		for (auto i = l.GetBegin(); i != l.GetEnd(); ++i, ++cnt)
+		{
+			assert(*i, cnt);
+		}
+	}
+}
+
+BENCHMARK(BM_StdListIteration)->Iterations(10000);
+BENCHMARK(BM_SgeListIteration)->Iterations(10000);
---
+++ Benchmark/BenchmarkCommon/BenchmarkVector.hpp
@@ -0,0 +1,151 @@
+﻿/*
+Copyright 2021 creatorlxd
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+#pragma once
+#include <benchmark/benchmark.h>
+#include "Container/Vector.hpp"
+#include <vector>
+#include <cassert>
+
+void BM_StdVectorCreate(benchmark::State& state)
+{
+	for (auto _ : state)
+	{
+		std::vector<int> v;
+	}
+}
+
+void BM_SgeVectorCreate(benchmark::State& state)
+{
+	for (auto _ : state)
+	{
+		SpaceGameEngine::Vector<int> v;
+	}
+}
+
+BENCHMARK(BM_StdVectorCreate)->Iterations(1000000);
+BENCHMARK(BM_SgeVectorCreate)->Iterations(1000000);
+
+inline const int vector_test_size = 1024;
+
+void BM_StdVectorPushBack(benchmark::State& state)
+{
+	for (auto _ : state)
+	{
+		std::vector<int> v;
+		for (int i = 0; i < vector_test_size; ++i)
+		{
+			v.push_back(i);
+		}
+	}
+}
+
+void BM_SgeVectorPushBack(benchmark::State& state)
+{
+	for (auto _ : state)
+	{
+		SpaceGameEngine::Vector<int> v;
+		for (int i = 0; i < vector_test_size; ++i)
+		{
+			v.PushBack(i);
+		}
+	}
+}
+
+BENCHMARK(BM_StdVectorPushBack)->Iterations(1000000);
+BENCHMARK(BM_SgeVectorPushBack)->Iterations(1000000);
+
+void BM_StdVectorInsert(benchmark::State& state)
+{
+	for (auto _ : state)
+	{
+		std::vector<int> v{0};
+		for (int i = 0; i < vector_test_size; ++i)
+		{
+			v.insert(v.begin() + 1, i);
+		}
+	}
+}
+
+void BM_SgeVectorInsert(benchmark::State& state)
+{
+	for (auto _ : state)
+	{
+		SpaceGameEngine::Vector<int> v{0};
+		for (int i = 0; i < vector_test_size; ++i)
+		{
+			v.Insert(v.GetBegin() + 1, i);
+		}
+	}
+}
+
+BENCHMARK(BM_StdVectorInsert)->Iterations(10000);
+BENCHMARK(BM_SgeVectorInsert)->Iterations(10000);
+
+void BM_StdVectorRemove(benchmark::State& state)
+{
+	for (auto _ : state)
+	{
+		std::vector<int> v(vector_test_size, 1);
+		for (int i = 0; i < vector_test_size - 1; ++i)
+		{
+			v.erase(v.begin() + 1);
+		}
+	}
+}
+
+void BM_SgeVectorRemove(benchmark::State& state)
+{
+	for (auto _ : state)
+	{
+		SpaceGameEngine::Vector<int> v(vector_test_size, 1);
+		for (int i = 0; i < vector_test_size - 1; ++i)
+		{
+			v.Remove(v.GetBegin() + 1);
+		}
+	}
+}
+
+BENCHMARK(BM_StdVectorRemove)->Iterations(10000);
+BENCHMARK(BM_SgeVectorRemove)->Iterations(10000);
+
+void BM_StdVectorIteration(benchmark::State& state)
+{
+	for (auto _ : state)
+	{
+		std::vector<int> v(vector_test_size, 1);
+		int cnt = 0;
+		for (auto i = v.begin(); i != v.end(); ++i, ++cnt)
+		{
+			assert(*i, cnt);
+		}
+	}
+}
+
+void BM_SgeVectorIteration(benchmark::State& state)
+{
+	for (auto _ : state)
+	{
+		SpaceGameEngine::Vector<int> v(vector_test_size, 1);
+		int cnt = 0;
+		for (auto i = v.GetBegin(); i != v.GetEnd(); ++i, ++cnt)
+		{
+			assert(*i, cnt);
+		}
+	}
+}
+
+BENCHMARK(BM_StdVectorIteration)->Iterations(10000);
+BENCHMARK(BM_SgeVectorIteration)->Iterations(10000);
