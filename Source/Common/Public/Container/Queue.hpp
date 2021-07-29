3fbceae942346b34cfce28921bff8622cc35ca2d
creatorlxd <1461637345@qq.com>
2021-07-29 16:08:57 +0800

    feat : add Queue

--- Source/Common/Public/Container/Queue.hpp
+++ Source/Common/Public/Container/Queue.hpp
@@ -14,3 +14,178 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 #pragma once
+#include "Vector.hpp"
+#include "List.hpp"
+
+namespace SpaceGameEngine
+{
+	/*!
+	@ingroup Common
+	@{
+	*/
+
+	template<typename T, typename Allocator = DefaultAllocator, template<typename, typename> class Implement = List>
+	class Queue
+	{
+	public:
+		using ValueType = T;
+		using AllocatorType = Allocator;
+		using ImplementType = Implement<T, Allocator>;
+
+		struct EmptyQueueError
+		{
+			inline static const TChar sm_pContent[] = SGE_TSTR("The Queue is empty");
+			inline static bool Judge(SizeType size)
+			{
+				return size == 0;
+			}
+		};
+
+		template<typename _T, typename _Allocator, template<typename, typename> class _Implement>
+		friend class Queue;
+
+	public:
+		inline Queue()
+			: m_Implement()
+		{
+		}
+
+		inline Queue(const Queue& s)
+			: m_Implement(s.m_Implement)
+		{
+		}
+
+		inline Queue(Queue&& s)
+			: m_Implement(std::move(s.m_Implement))
+		{
+		}
+
+		inline Queue& operator=(const Queue& s)
+		{
+			SGE_ASSERT(SelfAssignmentError, this, &s);
+			m_Implement = s.m_Implement;
+			return *this;
+		}
+
+		inline Queue& operator=(Queue&& s)
+		{
+			SGE_ASSERT(SelfAssignmentError, this, &s);
+			m_Implement = std::move(s.m_Implement);
+			return *this;
+		}
+
+		template<typename OtherAllocator>
+		inline Queue(const Queue<T, OtherAllocator, Implement>& s)
+			: m_Implement(s.m_Implement)
+		{
+		}
+
+		template<typename OtherAllocator>
+		inline Queue(Queue<T, OtherAllocator, Implement>&& s)
+			: m_Implement(std::move(s.m_Implement))
+		{
+		}
+
+		template<typename OtherAllocator>
+		inline Queue& operator=(const Queue<T, OtherAllocator, Implement>& s)
+		{
+			m_Implement = s.m_Implement;
+			return *this;
+		}
+
+		template<typename OtherAllocator>
+		inline Queue& operator=(Queue<T, OtherAllocator, Implement>&& s)
+		{
+			m_Implement = std::move(s.m_Implement);
+			return *this;
+		}
+
+		inline Queue(SizeType size, const T& val)
+			: m_Implement(size, val)
+		{
+		}
+
+		template<typename AnotherIteratorType, typename = std::enable_if_t<IsSequentialIterator<AnotherIteratorType>::Value, bool>>
+		inline Queue(const AnotherIteratorType& begin, const AnotherIteratorType& end)
+			: m_Implement(begin, end)
+		{
+		}
+
+		inline Queue(std::initializer_list<T> ilist)
+			: m_Implement(ilist)
+		{
+		}
+
+		inline Queue(const ImplementType& c)
+			: m_Implement(c)
+		{
+		}
+
+		inline Queue(ImplementType&& c)
+			: m_Implement(std::move(c))
+		{
+		}
+
+		template<typename OtherAllocator>
+		inline Queue(const Implement<T, OtherAllocator>& c)
+			: m_Implement(c)
+		{
+		}
+
+		template<typename OtherAllocator>
+		inline Queue(Implement<T, OtherAllocator>&& c)
+			: m_Implement(std::move(c))
+		{
+		}
+
+		inline SizeType GetSize() const
+		{
+			return m_Implement.GetSize();
+		}
+
+		inline ImplementType& GetImplement()
+		{
+			return m_Implement;
+		}
+
+		inline const ImplementType& GetImplement() const
+		{
+			return m_Implement;
+		}
+
+		inline void Push(const T& val)
+		{
+			m_Implement.PushBack(val);
+		}
+
+		inline void Push(T&& val)
+		{
+			m_Implement.PushBack(std::move(val));
+		}
+
+		inline void Pop()
+		{
+			SGE_ASSERT(EmptyQueueError, m_Implement.GetSize());
+			m_Implement.PopFront();
+		}
+
+		inline T& GetFront()
+		{
+			SGE_ASSERT(EmptyQueueError, m_Implement.GetSize());
+			return *(m_Implement.GetBegin());
+		}
+
+		inline const T& GetFront() const
+		{
+			SGE_ASSERT(EmptyQueueError, m_Implement.GetSize());
+			return *(m_Implement.GetConstBegin());
+		}
+
+	private:
+		ImplementType m_Implement;
+	};
+
+	/*!
+	@}
+	*/
+}
--- Test/TestCommon/TestQueue.hpp
+++ Test/TestCommon/TestQueue.hpp
@@ -16,3 +16,131 @@ limitations under the License.
 #pragma once
 #include "gtest/gtest.h"
 #include "Container/Queue.hpp"
+
+using namespace SpaceGameEngine;
+
+struct test_queue_object
+{
+	std::function<void(test_queue_object&)> rel_func;
+	test_queue_object()
+		: val(0), rel_func([](test_queue_object&) {})
+	{
+	}
+	test_queue_object(int v)
+		: val(v), rel_func([](test_queue_object&) {})
+	{
+	}
+	test_queue_object(int v, const std::function<void(test_queue_object&)>& func)
+		: val(v), rel_func(func)
+	{
+	}
+	test_queue_object(const test_queue_object& o) noexcept
+		: val(o.val), rel_func(o.rel_func)
+	{
+	}
+	test_queue_object(test_queue_object&& o) noexcept
+		: val(o.val), rel_func(std::move(o.rel_func))
+	{
+		o.rel_func = [](test_queue_object&) {};
+	}
+	~test_queue_object()
+	{
+		rel_func(*this);
+	}
+	int val;
+
+	test_queue_object& operator=(const test_queue_object& o)
+	{
+		val = o.val;
+		rel_func = o.rel_func;
+		return *this;
+	}
+
+	test_queue_object& operator=(test_queue_object&& o)
+	{
+		val = o.val;
+		rel_func = std::move(o.rel_func);
+		o.rel_func = [](test_queue_object&) {};
+		return *this;
+	}
+
+	bool operator<(const test_queue_object& o) const
+	{
+		return val < o.val;
+	}
+
+	bool operator==(const test_queue_object& o) const
+	{
+		return val == o.val;
+	}
+
+	bool operator!=(const test_queue_object& o) const
+	{
+		return val != o.val;
+	}
+};
+
+TEST(Queue, InstanceTest)
+{
+	Queue<int> q1;
+	Queue<int, StdAllocator> q2({1, 2, 3});
+	const Queue<int, DefaultAllocator> q3(10, 1);
+
+	ASSERT_EQ(q1.GetSize(), 0);
+	ASSERT_EQ(q2.GetSize(), 3);
+	ASSERT_EQ(q3.GetSize(), 10);
+
+	List<int, StdAllocator>& l2 = q2.GetImplement();
+	ASSERT_EQ(l2.GetSize(), 3);
+	ASSERT_EQ(*l2.GetBegin(), 1);
+	ASSERT_EQ(*(l2.GetBegin() + 1), 2);
+	ASSERT_EQ(*(l2.GetBegin() + 2), 3);
+
+	const List<int>& v3 = q3.GetImplement();
+	ASSERT_EQ(v3.GetSize(), 10);
+	for (auto i = v3.GetConstBegin(); i != v3.GetConstEnd(); ++i)
+		ASSERT_EQ(*i, 1);
+}
+
+TEST(Queue, PushAndPopTest)
+{
+	const int test_size = 1000;
+	int val_pool[test_size];
+	memset(val_pool, 0, sizeof(val_pool));
+	auto val_rel_func = [&](test_queue_object& o) {
+		val_pool[o.val] += 1;
+	};
+
+	Queue<test_queue_object>* pq = new Queue<test_queue_object>();
+	const Queue<test_queue_object>* pcq = pq;
+
+	for (int i = 0; i < test_size; ++i)
+	{
+		test_queue_object tbuf(i, val_rel_func);
+		pq->Push(tbuf);
+		ASSERT_EQ(pq->GetFront().val, 0);
+	}
+	ASSERT_EQ(pq->GetSize(), test_size);
+
+	for (int i = 0; i < test_size; ++i)
+	{
+		ASSERT_EQ(pcq->GetFront().val, i);
+		pq->Pop();
+		ASSERT_EQ(pq->GetSize(), test_size - 1 - i);
+	}
+	ASSERT_EQ(pq->GetSize(), 0);
+
+	for (int i = 0; i < test_size; ++i)
+	{
+		test_queue_object tbuf(i, val_rel_func);
+		pq->Push(std::move(tbuf));
+		ASSERT_EQ(pq->GetFront().val, 0);
+	}
+	ASSERT_EQ(pq->GetSize(), test_size);
+
+	delete pq;
+	for (int i = 0; i < test_size; ++i)
+	{
+		ASSERT_EQ(val_pool[i], 3);
+	}
+}
