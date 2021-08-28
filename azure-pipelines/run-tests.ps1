5dd2f24091c08369e16775b29fd7afe6aa7bb8e9
creatorlxd <1461637345@qq.com>
2021-08-28 20:42:00 +0800

    fix azure

--- azure-pipelines/run-tests.ps1
+++ azure-pipelines/run-tests.ps1
@@ -6,8 +6,8 @@ $config = $args[1]
 (Get-ChildItem -Directory) | foreach {
     $testName = $_.Name
     cd $testName
-    pwd
-    &"..\Binary\$testName\$arch\$config\$testName.exe" "--gtest_output=xml:$testName-REPORT.xml"
+    Get-Location
+    #&"..\Binary\$testName\$arch\$config\$testName.exe" "--gtest_output=xml:$testName-REPORT.xml"
     cd ..
 }
 
