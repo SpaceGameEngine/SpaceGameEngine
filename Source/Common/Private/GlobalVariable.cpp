ce9c4ef1a084056f5951229e9cae6bb512079021
creatorlxd <1461637345@qq.com>
2021-08-30 15:20:47 +0800

    feat : add GlobalVariable fix : Vector :: operator =

---
+++ Source/Common/Private/GlobalVariable.cpp
@@ -0,0 +1,49 @@
+﻿/*
+Copyright 2021 creatorlxd
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+#include "GlobalVariable.h"
+#include "Error.h"
+
+void SpaceGameEngine::GlobalVariableCore::OnRelease()
+{
+}
+
+SpaceGameEngine::GlobalVariableManager::~GlobalVariableManager()
+{
+	while (m_Content.GetSize() != 0)
+	{
+		GlobalVariableCore* ptr = m_Content.GetTop();
+		m_Content.Pop();
+		ptr->OnRelease();
+	}
+}
+
+SpaceGameEngine::GlobalVariableManager& SpaceGameEngine::GlobalVariableManager::GetSingleton()
+{
+	static GlobalVariableManager g_GlobalVariableManager;
+	return g_GlobalVariableManager;
+}
+
+SpaceGameEngine::GlobalVariableManager::GlobalVariableManager()
+{
+}
+
+void SpaceGameEngine::GlobalVariableManager::Add(GlobalVariableCore* ptr)
+{
+	SGE_ASSERT(NullPointerError, ptr);
+	RecursiveLock lock(m_Mutex);
+	lock.Lock();
+	m_Content.Push(ptr);
+}
---
+++ Source/Common/Public/GlobalVariable.h
@@ -0,0 +1,90 @@
+﻿/*
+Copyright 2021 creatorlxd
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+#pragma once
+#include "CommonAPI.h"
+#include "Container/Stack.hpp"
+#include "Concurrent/Lock.h"
+
+namespace SpaceGameEngine
+{
+	/*!
+	@ingroup Common
+	@{
+	*/
+
+	class COMMON_API GlobalVariableCore
+	{
+	public:
+		friend class GlobalVariableManager;
+
+	private:
+		virtual void OnRelease();
+	};
+
+	template class COMMON_API List<GlobalVariableCore*, StdAllocator>;
+	template class COMMON_API Stack<GlobalVariableCore*, StdAllocator>;
+
+	class COMMON_API GlobalVariableManager
+	{
+	public:
+		~GlobalVariableManager();
+
+		static GlobalVariableManager& GetSingleton();
+
+	private:
+		GlobalVariableManager();
+		void Add(GlobalVariableCore* ptr);
+
+	private:
+		Mutex m_Mutex;
+		Stack<GlobalVariableCore*, StdAllocator> m_Content;
+	};
+
+	template<typename T, typename Allocator = DefaultAllocator>
+	class GlobalVariable : public GlobalVariableCore
+	{
+	public:
+		template<typename... Args>
+		inline GlobalVariable(Args&&... args)
+		{
+			m_pContent = Allocator::template New<T>(std::forward<Args>(args)...);
+			GlobalVariableManager::GetSingleton().Add(this);
+		}
+
+		inline T& Get()
+		{
+			return *m_pContent;
+		}
+
+		inline const T& Get() const
+		{
+			return *m_pContent;
+		}
+
+	private:
+		inline virtual void OnRelease() override
+		{
+			Allocator::Delete(m_pContent);
+		}
+
+	private:
+		T* m_pContent;
+	};
+
+	/*!
+	@}
+	*/
+}
