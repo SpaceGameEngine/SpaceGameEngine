4f9439913da858fb5131c74abc14182fc78286a8
creatorlxd <1461637345@qq.com>
2022-04-11 10:49:36 +0800

    feat : add lexer file

--- Source/SpaceLanguage/Private/Lexer.cpp
+++ Source/SpaceLanguage/Private/Lexer.cpp
@@ -13,19 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
-#include "gtest/gtest.h"
-#include "Bootstrap.h"
-#include "SGEString.hpp"
-#include "Module/ModuleManager.h"
-#include "TestVirtualMachine.hpp"
-#include "TestLexer.hpp"
-#include "TestAssembler.hpp"
+#include "Lexer.h"
 
-SpaceGameEngine::Bootstrap bootstrap;
-
-int main(int argc, char** argv)
-{
-	SpaceGameEngine::ModuleManager::GetSingleton().LoadModule(SGE_STR("SpaceLanguage"));
-	::testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
-}
+using namespace SpaceGameEngine;
+using namespace SpaceGameEngine::SpaceLanguage;
--- Test/TestSpaceLanguage/TestLexer.hpp
+++ Test/TestSpaceLanguage/TestLexer.hpp
@@ -13,19 +13,9 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 */
+#pragma once
+#include "Lexer.h"
 #include "gtest/gtest.h"
-#include "Bootstrap.h"
-#include "SGEString.hpp"
-#include "Module/ModuleManager.h"
-#include "TestVirtualMachine.hpp"
-#include "TestLexer.hpp"
-#include "TestAssembler.hpp"
 
-SpaceGameEngine::Bootstrap bootstrap;
-
-int main(int argc, char** argv)
-{
-	SpaceGameEngine::ModuleManager::GetSingleton().LoadModule(SGE_STR("SpaceLanguage"));
-	::testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
-}
+using namespace SpaceGameEngine;
+using namespace SpaceGameEngine::SpaceLanguage;
