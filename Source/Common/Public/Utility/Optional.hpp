873d8ce46b21629bc6eaef388a1c7499635a0806
creatorlxd <1461637345@qq.com>
2022-12-11 19:57:26 +0800

    feat : add optional

--- Source/Common/Public/Utility/Optional.hpp
+++ Source/Common/Public/Utility/Optional.hpp
@@ -15,6 +15,8 @@ limitations under the License.
 */
 #pragma once
 #include "TypeDefinition.hpp"
+#include "Error.h"
+#include "Meta/Trait.hpp"
 
 /*!
 @ingroup Common
@@ -23,7 +25,190 @@ limitations under the License.
 
 namespace SpaceGameEngine
 {
+	struct OptionalIsEmptyError;
+	struct OptionalIsNotEmptyError;
 
+	namespace OptionalTag
+	{
+		struct EmptyOptionalTag
+		{
+		};
+
+		inline constexpr const EmptyOptionalTag EmptyOptional;
+
+		struct DefaultValueTag
+		{
+		};
+
+		inline constexpr const DefaultValueTag DefaultValue;
+	}
+
+	template<typename T>
+	class Optional
+	{
+	public:
+		inline Optional()
+			: m_HasValue(false)
+		{
+		}
+
+		template<typename... Args,
+				 typename = std::enable_if_t<
+					 !(sizeof...(Args) == 1 &&
+					   (std::is_same_v<RemoveCVRefType<Args>, Optional> && ...)),
+					 void>>
+		inline Optional(Args&&... args)
+			: m_HasValue(true)
+		{
+			new (m_Value) T(std::forward<Args>(args)...);
+		}
+
+		inline Optional(OptionalTag::EmptyOptionalTag)
+			: m_HasValue(false)
+		{
+		}
+
+		inline Optional(OptionalTag::DefaultValueTag)
+			: m_HasValue(true)
+		{
+			new (m_Value) T();
+		}
+
+		inline ~Optional()
+		{
+			if (m_HasValue)
+				reinterpret_cast<T*>(m_Value)->~T();
+		}
+
+		inline Optional(const Optional& opt)
+			: m_HasValue(opt.m_HasValue)
+		{
+			if (m_HasValue)
+				new (m_Value) T(opt.Get());
+		}
+
+		inline Optional(Optional&& opt)
+			: m_HasValue(opt.m_HasValue)
+		{
+			if (m_HasValue)
+			{
+				new (m_Value) T(std::move(opt.Get()));
+				opt.Clear();
+			}
+		}
+
+		inline Optional& operator=(const Optional& opt)
+		{
+			SGE_ASSERT(SelfAssignmentError, this, &opt);
+
+			if (m_HasValue && opt.m_HasValue)
+				Get() = opt.Get();
+			else if (m_HasValue && (!opt.m_HasValue))
+				Clear();
+			else if ((!m_HasValue) && opt.m_HasValue)
+				Set(opt.Get());
+
+			return *this;
+		}
+
+		inline Optional& operator=(Optional&& opt)
+		{
+			SGE_ASSERT(SelfAssignmentError, this, &opt);
+
+			if (m_HasValue && opt.m_HasValue)
+			{
+				Get() = std::move(opt.Get());
+				opt.Clear();
+			}
+			else if (m_HasValue && (!opt.m_HasValue))
+				Clear();
+			else if ((!m_HasValue) && opt.m_HasValue)
+			{
+				Set(std::move(opt.Get()));
+				opt.Clear();
+			}
+
+			return *this;
+		}
+
+		inline bool HasValue() const
+		{
+			return m_HasValue;
+		}
+
+		inline T& Get()
+		{
+			SGE_ASSERT(OptionalIsEmptyError, *this);
+			return *reinterpret_cast<T*>(m_Value);
+		}
+
+		inline const T& Get() const
+		{
+			SGE_ASSERT(OptionalIsEmptyError, *this);
+			return *reinterpret_cast<const T*>(m_Value);
+		}
+
+		template<typename... Args>
+		inline void Set(Args&&... args)
+		{
+			SGE_ASSERT(OptionalIsNotEmptyError, *this);
+			m_HasValue = true;
+			new (m_Value) T(std::forward<Args>(args)...);
+		}
+
+		inline void Clear()
+		{
+			if (m_HasValue)
+			{
+				reinterpret_cast<T*>(m_Value)->~T();
+				m_HasValue = false;
+			}
+		}
+
+		inline bool operator==(const Optional& opt) const
+		{
+			if (m_HasValue != opt.m_HasValue)
+				return false;
+			else if (m_HasValue)
+				return Get() == opt.Get();
+			else
+				return true;
+		}
+
+		inline bool operator!=(const Optional& opt) const
+		{
+			if (m_HasValue != opt.m_HasValue)
+				return true;
+			else if (m_HasValue)
+				return Get() != opt.Get();
+			else
+				return false;
+		}
+
+	private:
+		bool m_HasValue;
+		Byte m_Value[sizeof(T)];
+	};
+
+	struct OptionalIsEmptyError
+	{
+		inline static const ErrorMessageChar sm_pContent[] = SGE_ESTR("Optional is empty");
+		template<typename T>
+		inline static bool Judge(const Optional<T>& optional)
+		{
+			return !optional.HasValue();
+		}
+	};
+
+	struct OptionalIsNotEmptyError
+	{
+		inline static const ErrorMessageChar sm_pContent[] = SGE_ESTR("Optional is not empty");
+		template<typename T>
+		inline static bool Judge(const Optional<T>& optional)
+		{
+			return optional.HasValue();
+		}
+	};
 }
 
 /*!
--- Test/TestCommon/TestUtility.hpp
+++ Test/TestCommon/TestUtility.hpp
@@ -22,6 +22,7 @@ limitations under the License.
 #include "Utility/Endian.h"
 #include "Utility/DebugInformation.h"
 #include "Utility/FixedSizeBuffer.hpp"
+#include "Utility/Optional.hpp"
 #include "SGEString.hpp"
 #include "gtest/gtest.h"
 
@@ -134,7 +135,7 @@ TEST(ControllableObject, CopyTest)
 	ASSERT_EQ(test10.Get(), 9);
 }
 
-TEST(ControllableObject, ComparasionTest)
+TEST(ControllableObject, EqualityTest)
 {
 	ControllableObject<int> test(1), test2(2), test3(1);
 	ASSERT_FALSE(test == test2);
@@ -226,7 +227,7 @@ TEST(DebugInformation, DebugInformationTest)
 	TString<> funcname(di.m_pFunctionName);
 	ASSERT_NE(filename.Find(SGE_TSTR("TestUtility.hpp"), filename.GetConstBegin(), filename.GetConstEnd()), filename.GetConstEnd());
 	ASSERT_NE(funcname.Find(SGE_TSTR("TestDebugInformation"), funcname.GetConstBegin(), funcname.GetConstEnd()), funcname.GetConstEnd());
-	ASSERT_EQ(di.m_LineNumber, 219);
+	ASSERT_EQ(di.m_LineNumber, 220);
 }
 
 TEST(FixedSizeBuffer, InstanceTest)
@@ -436,4 +437,220 @@ TEST(FixedSizeBuffer, AnotherAllocatorMoveOperatorTest)
 	ASSERT_EQ(buffer2.GetSize(), sizeof(test_data));
 	ASSERT_EQ(buffer2.GetFreeSize(), 32 - sizeof(test_data));
 	ASSERT_EQ(*(UInt32*)buffer2.GetData(), test_data);
+}
+
+TEST(Optional, InstanceTest)
+{
+	Optional<test_controllable_object_class> empty_opt;
+	ASSERT_FALSE(empty_opt.HasValue());
+
+	Optional<test_controllable_object_class> opt1{test_controllable_object_class()};
+	ASSERT_TRUE(opt1.HasValue());
+	ASSERT_EQ(opt1.Get().i, 3);
+
+	bool has_destruct = false;
+	{
+		const Optional<test_auto_release_buffer_class> opt2([&]() {
+			has_destruct = true;
+		});
+		ASSERT_TRUE(opt2.HasValue());
+		ASSERT_FALSE(has_destruct);
+	}
+	ASSERT_TRUE(has_destruct);
+
+	Optional<test_controllable_object_class> opt3(OptionalTag::EmptyOptional);
+	ASSERT_FALSE(opt3.HasValue());
+
+	Optional<test_controllable_object_class> opt4(OptionalTag::DefaultValue);
+	ASSERT_TRUE(opt4.HasValue());
+	ASSERT_EQ(opt4.Get().i, 1);
+}
+
+TEST(Optional, CopyConstructionTest)
+{
+	Optional<test_controllable_object_class> opt1;
+	ASSERT_FALSE(opt1.HasValue());
+
+	Optional<test_controllable_object_class> opt2(OptionalTag::DefaultValue);
+	ASSERT_TRUE(opt2.HasValue());
+	ASSERT_EQ(opt2.Get().i, 1);
+
+	Optional<test_controllable_object_class> opt3(opt1);
+	ASSERT_FALSE(opt3.HasValue());
+	ASSERT_FALSE(opt1.HasValue());
+
+	Optional<test_controllable_object_class> opt4(opt2);
+	ASSERT_TRUE(opt4.HasValue());
+	ASSERT_EQ(opt4.Get().i, 2);
+	ASSERT_TRUE(opt2.HasValue());
+	ASSERT_EQ(opt2.Get().i, 1);
+}
+
+TEST(Optional, MoveConstructionTest)
+{
+	Optional<test_controllable_object_class> opt1;
+	ASSERT_FALSE(opt1.HasValue());
+
+	Optional<test_controllable_object_class> opt2(OptionalTag::DefaultValue);
+	ASSERT_TRUE(opt2.HasValue());
+	ASSERT_EQ(opt2.Get().i, 1);
+
+	Optional<test_controllable_object_class> opt3(std::move(opt1));
+	ASSERT_FALSE(opt3.HasValue());
+	ASSERT_FALSE(opt1.HasValue());
+
+	Optional<test_controllable_object_class> opt4(std::move(opt2));
+	ASSERT_TRUE(opt4.HasValue());
+	ASSERT_EQ(opt4.Get().i, 3);
+	ASSERT_FALSE(opt2.HasValue());
+}
+
+TEST(Optional, CopyAssignmentTest)
+{
+	Optional<test_controllable_object_class> opt1;
+	ASSERT_FALSE(opt1.HasValue());
+
+	Optional<test_controllable_object_class> opt2(OptionalTag::DefaultValue);
+	ASSERT_TRUE(opt2.HasValue());
+	ASSERT_EQ(opt2.Get().i, 1);
+
+	Optional<test_controllable_object_class> opt3;
+	ASSERT_FALSE(opt3.HasValue());
+
+	Optional<test_controllable_object_class> opt4;
+	ASSERT_FALSE(opt4.HasValue());
+
+	opt4 = opt1;
+
+	ASSERT_FALSE(opt4.HasValue());
+	ASSERT_FALSE(opt1.HasValue());
+
+	opt4 = opt2;
+
+	ASSERT_TRUE(opt4.HasValue());
+	ASSERT_EQ(opt4.Get().i, 2);
+	ASSERT_TRUE(opt2.HasValue());
+	ASSERT_EQ(opt2.Get().i, 1);
+
+	opt4 = opt2;
+
+	ASSERT_TRUE(opt4.HasValue());
+	ASSERT_EQ(opt4.Get().i, 4);
+	ASSERT_TRUE(opt2.HasValue());
+	ASSERT_EQ(opt2.Get().i, 1);
+
+	opt4 = opt3;
+
+	ASSERT_FALSE(opt4.HasValue());
+	ASSERT_FALSE(opt3.HasValue());
+}
+
+TEST(Optional, MoveAssignmentTest)
+{
+	Optional<test_controllable_object_class> opt1;
+	ASSERT_FALSE(opt1.HasValue());
+
+	Optional<test_controllable_object_class> opt2(OptionalTag::DefaultValue);
+	ASSERT_TRUE(opt2.HasValue());
+	ASSERT_EQ(opt2.Get().i, 1);
+
+	Optional<test_controllable_object_class> opt3(OptionalTag::DefaultValue);
+	ASSERT_TRUE(opt3.HasValue());
+	ASSERT_EQ(opt3.Get().i, 1);
+
+	Optional<test_controllable_object_class> opt4;
+	ASSERT_FALSE(opt4.HasValue());
+
+	Optional<test_controllable_object_class> opt5;
+	ASSERT_FALSE(opt5.HasValue());
+
+	opt5 = std::move(opt1);
+
+	ASSERT_FALSE(opt5.HasValue());
+	ASSERT_FALSE(opt1.HasValue());
+
+	opt5 = std::move(opt2);
+
+	ASSERT_TRUE(opt5.HasValue());
+	ASSERT_EQ(opt5.Get().i, 3);
+	ASSERT_FALSE(opt2.HasValue());
+
+	opt5 = std::move(opt3);
+
+	ASSERT_TRUE(opt5.HasValue());
+	ASSERT_EQ(opt5.Get().i, 5);
+	ASSERT_FALSE(opt3.HasValue());
+
+	opt5 = std::move(opt4);
+
+	ASSERT_FALSE(opt5.HasValue());
+	ASSERT_FALSE(opt4.HasValue());
+}
+
+TEST(Optional, GetTest)
+{
+	Optional<int> opt(1);
+	ASSERT_EQ(opt.Get(), 1);
+	ASSERT_TRUE((std::is_same_v<decltype(opt.Get()), int&>));
+
+	const Optional<int> copt(2);
+	ASSERT_EQ(copt.Get(), 2);
+	ASSERT_TRUE((std::is_same_v<decltype(copt.Get()), const int&>));
+}
+
+TEST(Optional, SetTest)
+{
+	Optional<test_controllable_object_class> opt;
+	ASSERT_FALSE(opt.HasValue());
+
+	opt.Set();
+
+	ASSERT_TRUE(opt.HasValue());
+	ASSERT_EQ(opt.Get().i, 1);
+}
+
+TEST(Optional, ClearTest)
+{
+	Optional<test_controllable_object_class> opt(OptionalTag::DefaultValue);
+	ASSERT_TRUE(opt.HasValue());
+	ASSERT_EQ(opt.Get().i, 1);
+
+	opt.Clear();
+
+	ASSERT_FALSE(opt.HasValue());
+
+	opt.Set(test_controllable_object_class());
+
+	ASSERT_TRUE(opt.HasValue());
+	ASSERT_EQ(opt.Get().i, 3);
+}
+
+TEST(Optional, EqualityTest)
+{
+	Optional<int> opt1;
+	ASSERT_FALSE(opt1.HasValue());
+
+	const Optional<int> opt2(0);
+	ASSERT_TRUE(opt2.HasValue());
+	ASSERT_EQ(opt2.Get(), 0);
+
+	Optional<int> opt3(1);
+	ASSERT_TRUE(opt3.HasValue());
+	ASSERT_EQ(opt3.Get(), 1);
+
+	ASSERT_TRUE(opt1 == opt1);
+	ASSERT_TRUE(opt2 == opt2);
+
+	ASSERT_FALSE(opt1 == opt2);
+	ASSERT_FALSE(opt2 == opt1);
+	ASSERT_FALSE(opt2 == opt3);
+	ASSERT_FALSE(opt3 == opt2);
+
+	ASSERT_TRUE(opt1 != opt2);
+	ASSERT_TRUE(opt2 != opt1);
+	ASSERT_TRUE(opt2 != opt3);
+	ASSERT_TRUE(opt3 != opt2);
+
+	ASSERT_FALSE(opt1 != opt1);
+	ASSERT_FALSE(opt2 != opt2);
 }
