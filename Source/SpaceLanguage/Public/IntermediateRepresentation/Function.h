91dcaec0b08b9bf74c1798cf95bcff0dd7ba515b
creatorlxd <1461637345@qq.com>
2023-03-30 16:40:39 +0800

    feat : add BasicBlockGraph file

--- Source/SpaceLanguage/Public/IntermediateRepresentation/Function.h
+++ Source/SpaceLanguage/Public/IntermediateRepresentation/Function.h
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 #pragma once
-#include "IntermediateRepresentation/BasicBlock.h"
+#include "IntermediateRepresentation/BasicBlockGraph.h"
 
 /*!
 @ingroup SpaceLanguage
