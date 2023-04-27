8d887dd848666bf0ac3cf5186b20d50c83729300
creatorlxd <1461637345@qq.com>
2023-04-27 21:21:03 +0800

    fix : remove useless BasicBlockGraph

--- Source/SpaceLanguage/Public/IntermediateRepresentation/Function.h
+++ Source/SpaceLanguage/Public/IntermediateRepresentation/Function.h
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 #pragma once
-#include "IntermediateRepresentation/BasicBlockGraph.h"
+#include "IntermediateRepresentation/BasicBlock.h"
 
 /*!
 @ingroup SpaceLanguage
